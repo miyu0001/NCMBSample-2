140a9a2c2295393b3d8af46f6a9bcb2f8ffd9aeb
Miyu Sato <62976492+miyu0001@users.noreply.github.com>
2021-04-29 20:46:22 +0900

    Update ViewController.swift

--- NCMBSample/ViewController.swift
+++ NCMBSample/ViewController.swift
@@ -21,75 +21,75 @@ class ViewController: UIViewController {
     @IBAction func save(_ sender: Any) {
         let object = NCMBObject(className:"Message")
         object?.setObject(SampleTextField.text, forKey: "text")
-        object?.saveInBackground({ (error) in
+        object?.saveInBackground{ (error) in
             if error != nil{
                 //エラーが発生したら
                 print("error")
-            }else{
+            }else {
                 //保存に成功した場合
                 print("success.navigationBarBackButtonHidden(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)")
             }
-        })
+        }
     }
     
     
     @IBAction func loadData(_ sender: Any) {
         let query = NCMBQuery(className: "Message")
-        query?.findObjectsInBackground({ (result, error) in
+        query?.findObjectsInBackground{ (result, error) in
             if error != nil{
                 //エラーの場合
                 print("error")
-            }else{
+            }else {
                 //うまく取得できた場合
                 let messages = result as! [NCMBObject]
                 let text = messages.last?.object(forKey:"text") as! String
                 
                 self.SampleTextField.text = text
             }
-        })
+        }
     }
     
     @IBAction func update() {
         let query = NCMBQuery(className: "Message")
         query?.whereKey("text", equalTo:"あいうえお")
-        query?.findObjectsInBackground({ (result, error) in
+        query?.findObjectsInBackground { (result, error) in
             if error != nil{
                 print("error")
-            }else{
+            }else {
                 let messages = result as! [NCMBObject]
                 let textobject = messages.first
                 textobject?.setObject("こんばんは", forKey: "text")
-                textobject?.saveInBackground({ (error) in
+                textobject?.saveInBackground{ (error) in
                     if error != nil {
                         print("error")
-                    }else{
+                    }else {
                         print("update success")
                     }
-                })
+                }
             }
-        })
+        }
     }
       
     
     //てすとと書いてあるところを削除する→できてない
     @IBAction func delete () {
-        let query = NCMBQuery(className: "message")
+        let query = NCMBQuery(className: "Message")
         query?.whereKey("text", equalTo: "てすと")
-        query?.findObjectsInBackground({ (result, error) in
+        query?.findObjectsInBackground{ (result, error) in
             if error != nil {
                 print(error)
-            }else{
+            }else {
                 let message = result as! [NCMBObject]
                 let textObject = message.first
-                textObject?.deleteInBackground({ (error) in
+                textObject?.deleteInBackground{ (error) in
                     if error != nil{
                         print(error)
-                    }else{
+                    }else {
                         print("Delete succeed")
                     }
-                })
+                }
             }
-        })
+        }
     }
     
     
